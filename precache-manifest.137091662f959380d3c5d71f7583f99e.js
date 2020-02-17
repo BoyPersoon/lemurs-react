6cd1e56abc06d52cf1ecf786d06673c452120a0c
Boy PErsoon <b.persoon@lemursdev.com>
2020-02-17 14:42:15 +0100

    Updates

---
+++ precache-manifest.137091662f959380d3c5d71f7583f99e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4fe3458720bcf85a2b248a84115cc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cdc9d2768e860f445b9",
+    "url": "/static/css/main.bd48c360.chunk.css"
+  },
+  {
+    "revision": "f2f11a8079bd75ac7dee",
+    "url": "/static/js/2.002581c1.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/static/js/2.002581c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdc9d2768e860f445b9",
+    "url": "/static/js/main.1c1a0742.chunk.js"
+  },
+  {
+    "revision": "c2ed8ef1dda681d55ae3",
+    "url": "/static/js/runtime-main.16a786f0.js"
+  },
+  {
+    "revision": "8413e6821efcc8cae15dd40508ce8e4e",
+    "url": "/static/media/hard-1.8413e682.jpg"
+  },
+  {
+    "revision": "2311defd3f69155647cc570c93616f2f",
+    "url": "/static/media/host-1.2311defd.jpg"
+  },
+  {
+    "revision": "13e1fa0495e7af55c066a4e04eea42cf",
+    "url": "/static/media/lemur-large-md-2.13e1fa04.jpg"
+  },
+  {
+    "revision": "ad7570e6ae6ee0189994149b1d44efd2",
+    "url": "/static/media/logo-circ-bg-w.ad7570e6.png"
+  },
+  {
+    "revision": "8214b3b2402d2d7b9aef55208970d57e",
+    "url": "/static/media/logo-circ.8214b3b2.png"
+  },
+  {
+    "revision": "57d243bb4a1819a52e9f336264e3cf11",
+    "url": "/static/media/logo-lemurs-text-w-min.57d243bb.png"
+  },
+  {
+    "revision": "7305313cacc0b41c74e556efa1441ab0",
+    "url": "/static/media/logo-text.7305313c.png"
+  },
+  {
+    "revision": "8e1251f0c7a487312a64b2d5dbf52248",
+    "url": "/static/media/pf_colina_dark-blue-s.8e1251f0.jpg"
+  },
+  {
+    "revision": "195fef452034505bfff06422d4dad77c",
+    "url": "/static/media/soft-1.195fef45.jpg"
+  }
+]);
